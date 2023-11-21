86ab7c98e1caf1e7200b38b52fbf06969a1e48d8
Perming1126 <perming@student.usm.my>
2023-11-22 03:54:07 +0800

    delete authentication and change mainactivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:payhackfinovators/pages/Authentication.dart';
 import 'package:payhackfinovators/pages/OnboardingSlider.dart';
 import 'package:payhackfinovators/pages/Payment.dart';
 import 'package:payhackfinovators/pages/Profile.dart';
@@ -37,7 +36,6 @@ class ProfileApp extends StatelessWidget {
           '/profile': (BuildContext context) => Profile(),
           '/history': (BuildContext context) => History(),
           '/scanner': (BuildContext context) => MyCustomWidget(),
-          '/authentication': (BuildContext context) => FingerprintAuth(),
           // '/scanner': (BuildContext context) => MyCustomWidget(),
         });
   }
